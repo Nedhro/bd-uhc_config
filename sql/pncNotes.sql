d200d31e37b7f46a34c2b730712cae8747f20c8f
rakibccj <rakibccj@gmail.com>
2019-04-27 15:38:23 +0600

    PNC Abdominal,Veginal Examination added

--- sql/pncNotes.sql
+++ sql/pncNotes.sql
@@ -27,5 +27,33 @@ call add_concept_answer(@pnc_oedemae, @oedemae_three_plus, 3);
 call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Jaundice", "Jaundice", "Numeric", "Finding", false);
 call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Others", "Others", "Text", "Misc", false);
 
+call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Fundal Height", "Fundal Height(Weeks)", "Numeric", "Finding", false);
+
+
+call add_concept(@pnc_abdominal, @s_name_id, @f_name_id, "PNC,Abdominal wound", "Abdominal wound", "Coded", "Misc", false);
+call add_concept(@pnc_abdominal_healthy_concept_id, @s_name_id, @f_name_id, "PNC,Healthy", "Healthy", "N/A", "Misc", false);
+call add_concept(@pnc_abdominal_infected_concept_id, @s_name_id, @f_name_id, "PNC,Infected", "Infected", "N/A", "Misc", false);
+
+call add_concept_answer(@pnc_abdominal, @pnc_abdominal_healthy_concept_id, 1);
+call add_concept_answer(@pnc_abdominal, @pnc_abdominal_infected_concept_id, 2);
+
+call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Abdomina Other", "Others", "Text", "Misc", false);
+
+call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Excesive Veginal bleeding", "Excesive Veginal bleeding", "Boolean", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Vaginal Discharge", "Vaginal Discharge", "Boolean", "Misc", false);
+
+call add_concept(@pnc_perineal_concept_id, @s_name_id, @f_name_id, "PNC,Perineal Tear Or Episiotomy Wound", "PNC,Perineal Tear/Episiotomy Wound", "Coded", "Misc", false);
+call add_concept(@pnc_perineal_healthy_concept_id, @s_name_id, @f_name_id, "PNC,Healthy", "Healthy", "N/A", "Misc", false);
+call add_concept(@pnc_perineal_infectedconcept_id, @s_name_id, @f_name_id, "PNC,Infected", "Infected", "N/A", "Misc", false);
+
+call add_concept_answer(@pnc_perineal_concept_id, @pnc_perineal_healthy_concept_id, 1);
+call add_concept_answer(@pnc_perineal_concept_id, @pnc_perineal_infectedconcept_id, 2);
+
+
+
+
+
+
+
 
 
