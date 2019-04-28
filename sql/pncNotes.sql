aeeec60cdf1633f85f851af60bafad7a93c72a7b
rakibccj <rakibccj@gmail.com>
2019-04-28 13:33:44 +0600

    PNC done with json

--- sql/pncNotes.sql
+++ sql/pncNotes.sql
@@ -8,10 +8,14 @@ call add_concept_answer(@pnc_delivery_outcome, @pnc_delivery_still_birth, 2);
 
 call add_concept(@pnc_active_manage, @s_name_id, @f_name_id, "PNC,AMTSL(Active Management of Third Stage of Labor) Given", "AMTSL(Active Management of Third Stage of Labor) Given", "Boolean", "Misc", false);
 
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Temperature", "Temperature(Deg F)", "Numeric", "Finding", false);
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Pulse", "Pulse(Beats Per Minute)", "Numeric", "Finding", false);
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Weight", "Weight", "Numeric", "Finding", false);
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Anemia", "Anemia", "Numeric", "Finding", false);
+call add_concept(@pnc_temperature_id, @s_name_id, @f_name_id, "PNC,Temperature", "Temperature(Deg F)", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_temperature_id, null, null, null);
+call add_concept(@pnc_pulse_concept_id, @s_name_id, @f_name_id, "PNC,Pulse", "Pulse(Beats Per Minute)", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_pulse_concept_id, null, null, null);
+call add_concept(@pnc_weight_concept_id, @s_name_id, @f_name_id, "PNC,Weight", "Weight", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_weight_concept_id, null, null, null);
+call add_concept(@pnc_anemia_concept_id, @s_name_id, @f_name_id, "PNC,Anemia", "Anemia", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_anemia_concept_id, null, null, null);
 
 
 call add_concept(@pnc_oedemae, @s_name_id, @f_name_id, "PNC,Oedema", "Oedema", "Coded", "Question", false);
@@ -24,11 +28,13 @@ call add_concept_answer(@pnc_oedemae, @oedemae_one_plus, 1);
 call add_concept_answer(@pnc_oedemae, @oedemae_two_plus, 2);
 call add_concept_answer(@pnc_oedemae, @oedemae_three_plus, 3);
 
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Jaundice", "Jaundice", "Numeric", "Finding", false);
+call add_concept(@pnc_jaundiceconcept_id, @s_name_id, @f_name_id, "PNC,Jaundice", "Jaundice", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_jaundiceconcept_id, null, null, null);
+
 call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Others", "Others", "Text", "Misc", false);
 
-call add_concept(@concept_id, @s_name_id, @f_name_id, "PNC,Fundal Height", "Fundal Height(Weeks)", "Numeric", "Finding", false);
-
+call add_concept(@pnc_fundal_heightconcept_id, @s_name_id, @f_name_id, "PNC,Fundal Height", "Fundal Height(Weeks)", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_fundal_heightconcept_id, null, null, null);
 
 call add_concept(@pnc_abdominal, @s_name_id, @f_name_id, "PNC,Abdominal wound", "Abdominal wound", "Coded", "Misc", false);
 call add_concept(@pnc_abdominal_healthy_concept_id, @s_name_id, @f_name_id, "PNC,Healthy", "Healthy", "N/A", "Misc", false);
@@ -56,6 +62,30 @@ call add_concept_numeric_db(@pnc_systolic_blood_pressure_concept_id, 110, 140, n
 call add_concept(@pnc_diastolic_blood_pressure_concept_id, @s_name_id, @f_name_id, "PNC,Diastolic Blood Pressure", "Diastolic Blood Pressure", "Numeric", "Finding", false);
 call add_concept_numeric_db(@pnc_diastolic_blood_pressure_concept_id, 80, 90, null);
 
+
+
+call add_concept(@pnc_baby_note_concept_id, @s_name_id, @f_name_id, "PNC, Baby Notes", "Baby Notes", "N/A", "Misc", true);
+call add_concept(@pnc_child_gender_concept_id, @s_name_id, @f_name_id, "PNC,Child Gender", "Child Gender", "Coded", "Misc", false);
+select @baby_male := concept_id from concept_name where name = "Baby male" and concept_name_type = "FULLY_SPECIFIED" order by date_created desc limit 1;
+select @baby_female := concept_id from concept_name where name = "Baby female" and concept_name_type = "FULLY_SPECIFIED" order by date_created desc limit 1;
+call add_concept_answer(@pnc_child_gender_concept_id, @baby_male, 1);
+call add_concept_answer(@pnc_child_gender_concept_id, @baby_female, 2);
+
+call add_concept(@pnc_baby_note_weight_concept_id, @s_name_id, @f_name_id, "PNC,Baby Note Weight", "Weight", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_baby_note_weight_concept_id, null, null, null);
+call add_concept(@pnc_baby_note_apgar_one_concept_id, @s_name_id, @f_name_id, "PNC, APGAR score at 1 Minutes", "APGAR score at 1 Minutes", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_baby_note_apgar_one_concept_id, null, null, null);
+call add_concept(@pnc_baby_note_apgar_five_concept_id, @s_name_id, @f_name_id, "PNC, APGAR score at 5 Minutes", "APGAR score at 5 Minutes", "Numeric", "Finding", false);
+call add_concept_numeric_db(@pnc_baby_note_apgar_five_concept_id, null, null, null);
+call add_concept(@pnc_date_and_time_of_born_concept_id, @s_name_id, @f_name_id, "PNC, Date and Time of Birth", "Date and Time of Birth", "Datetime", "Question", false);
+call add_concept(@pnc_baby_note_delivery_note_concept_id, @s_name_id, @f_name_id, "PNC, Delivary Baby Note", "Baby Note", "Text", "Misc", false);
+
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_child_gender_concept_id, 1);
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_baby_note_weight_concept_id, 2);
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_baby_note_apgar_one_concept_id, 3);
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_baby_note_apgar_five_concept_id, 4);
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_date_and_time_of_born_concept_id, 5);
+call add_concept_set_members(@pnc_baby_note_concept_id, @pnc_baby_note_delivery_note_concept_id, 6);
 
 
 
