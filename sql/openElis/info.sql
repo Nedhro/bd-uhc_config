4bfb90f7f7f8ddbfcaff0ec32e16e11b556b6833
rakibccj <rakibccj@gmail.com>
2019-05-09 14:33:31 +0600

    Added Hospital Info in Lab

---
+++ sql/openElis/info.sql
@@ -0,0 +1,2 @@
+update site_information set value = 'SAJIDA HOSPITAL' where name = 'SiteName';
+update site_information set value = 'Chhatgaon, Zinzira, Keraniganj, Dhaka-1310' where name = 'additional site info';
