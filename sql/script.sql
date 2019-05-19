759ade79a688ff0b2622c81db074d611c96a4441
Rakib Hasan <rakib_hasan6@yahoo.com>
2019-05-19 12:55:10 +0600

    Fixed location sql

--- sql/script.sql
+++ sql/script.sql
@@ -10,7 +10,7 @@ update location set parent_location=@location_id,state_province="Dhaka",county_d
 update location set parent_location=@location_id,state_province="Dhaka",county_district="Dhaka",address4="Keraniganj Paurashava",address5="Keraniganj" where name="OT";
 update location set parent_location=@location_id,state_province="Dhaka",county_district="Dhaka",address4="Keraniganj Paurashava",address5="Keraniganj" where name="Registration";
 update location set parent_location=@location_id,state_province="Dhaka",county_district="Dhaka",address4="Keraniganj Paurashava",address5="Keraniganj" where name="Vaccination";
-update location set parent_location=@location_id,state_province="Dhaka",county_district="Dhaka",address4="Keraniganj Paurashava",address5="Keraniganj" where location_id=@location_id;
+update location set state_province="Dhaka",county_district="Dhaka",address4="Keraniganj Paurashava",address5="Keraniganj" where location_id=@location_id;
 
 select @report_tag_id := location_tag_id from location_tag where name="Report Location";
 select @visit_tag_id := location_tag_id from location_tag where name="Visit Location";
