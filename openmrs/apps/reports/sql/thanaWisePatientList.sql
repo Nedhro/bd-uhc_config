476386b22ea35d01f2045bcd972c6eb3be7db0ce
rakibccj <rakibccj@gmail.com>
2019-07-27 16:01:52 +0600

    registration report generated bug fixed

--- openmrs/apps/reports/sql/thanaWisePatientList.sql
+++ openmrs/apps/reports/sql/thanaWisePatientList.sql
@@ -1,12 +1,4 @@
-select  pa.address5 as 'Thana',count(v.visit_id) as 'Visit Count' from visit v
-    inner join encounter e on e.visit_id=v.visit_id
-    INNER join location l on l.location_id = e.location_id
-    INNER JOIN location_tag_map lm on l.location_id= lm.location_id
-    inner JOIN location_tag lt on lt.location_tag_id = lm.location_tag_id and lt.name='Report Location'
-    inner join visit_type vt on vt.visit_type_id=v.visit_type_id
+select  pa.address5 as 'Thana',count(v.visit_id) as 'Total Visit' from visit as v
     inner join person_address as pa on pa.person_id = v.patient_id
-       where cast(v.date_created as DATE) between '#startDate#' AND '#endDate#'
-GROUP BY pa.address5 order by count(DISTINCT v.patient_id) desc;
-
-
-
+    and cast(v.date_created as DATE) between '#startDate#' AND '#endDate#'
+GROUP BY pa.address5 order by count(v.visit_id) desc;
--- openmrs/apps/reports/sql/visittypecount.sql
+++ openmrs/apps/reports/sql/visittypecount.sql
@@ -1,8 +1,4 @@
-select vt.name as 'Visit Type',count(v.visit_id) as 'Patient Count' from visit v
-  inner join encounter e on e.visit_id=v.visit_id
-  INNER join location l on l.location_id = e.location_id
-  INNER JOIN location_tag_map lm on l.location_id= lm.location_id
-  inner JOIN location_tag lt on lt.location_tag_id = lm.location_tag_id and lt.name='Report Location'
-  inner join visit_type vt on vt.visit_type_id=v.visit_type_id
+select vt.name as 'Visit Type',count(v.visit_id) as 'Patient Count' from visit as v
+ inner join visit_type as vt on vt.visit_type_id=v.visit_type_id
   and cast(v.date_created as DATE) between '#startDate#' AND '#endDate#'
-GROUP BY vt.name order by count(DISTINCT v.patient_id) desc;
+GROUP BY vt.name order by count(v.visit_id) desc;
