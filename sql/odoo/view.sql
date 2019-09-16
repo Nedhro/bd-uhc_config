8161f18a25499bf24204c57cf25e42c53f98bd1b
Rakib Hasan <rakib_hasan6@yahoo.com>
2019-09-16 15:20:42 +0600

    Added header view sql for odoo

---
+++ sql/odoo/view.sql
@@ -0,0 +1,6 @@
+update ir_ui_view set arch_db='<?xml version="1.0"?>
+<t t-name="report.external_layout_header">
+    <div class="header">
+        <div style="height:100px; width:100%; clear:both;"></div>
+    </div>
+</t>' where name = 'external_layout_header';
