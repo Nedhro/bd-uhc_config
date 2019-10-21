ef19af8cbba6ce44089f6de7b557795e6eb7c22a
Rakib Hasan <rakib.hasan786@gmail.com>
2019-10-21 09:43:10 +0600

    Added chartered accounting head csv in odoo

--- sql/openmrs/globalProperty.sql
+++ sql/openmrs/globalProperty.sql
@@ -1,16 +1,16 @@
 insert into global_property(property, property_value, uuid)
-VALUES("bahmni.sqlGet.providerDesignation", "select pa.value_reference as designation
+VALUES("bahmni.sqlGet.providerDesignation", 'select pa.value_reference as designation
 from provider_attribute as pa
 join provider p on pa.provider_id = p.provider_id
 join provider_attribute_type as pat on pa.attribute_type_id = pat.provider_attribute_type_id
-where p.uuid= ${providerUuid} and pat.name='Designation';", uuid());
+where p.uuid= ${providerUuid} and pat.name="Designation";', uuid());
 
 insert into global_property(property, property_value, uuid)
-VALUES("bahmni.sqlGet.providerDesignation2", "select pa.value_reference, pt.name from provider_attribute_type as pt join provider_attribute as pa on pt.provider_attribute_type_id = pa.attribute_type_id join provider as p on pa.provider_id = p.provider_id and p.uuid=${providerUuid};", uuid());
+VALUES("bahmni.sqlGet.providerDesignation2", 'select pa.value_reference, pt.name from provider_attribute_type as pt join provider_attribute as pa on pt.provider_attribute_type_id = pa.attribute_type_id join provider as p on pa.provider_id = p.provider_id and p.uuid=${providerUuid};', uuid());
 
 
 insert into global_property(property, property_value, uuid)
-VALUES("bahmni.sqlGet.orderUuid", "select p.uuid as uuid, pn.given_name, pn.middle_name, pn.family_name from orders as o join provider p on o.orderer = p.provider_id join person_name pn on pn.person_id = p.person_id where o.uuid=${orderUuid};", uuid());
+VALUES("bahmni.sqlGet.orderUuid", 'select p.uuid as uuid, pn.given_name, pn.middle_name, pn.family_name from orders as o join provider p on o.orderer = p.provider_id join person_name pn on pn.person_id = p.person_id where o.uuid=${orderUuid};', uuid());
 
 
 update global_property set property_value = '{"provider":["Doctor"]}' where property = "bahmni.relationshipTypeMap";
