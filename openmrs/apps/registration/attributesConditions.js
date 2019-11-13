8128174fa52972af87afe92ab7c24d1612629ea1
akter_rakib <rakibccj@gmail.com>
2019-11-13 14:33:57 +0600

    code clean

--- openmrs/apps/registration/attributesConditions.js
+++ openmrs/apps/registration/attributesConditions.js
@@ -5,24 +5,16 @@ var showOrHideReferredBySection = function (patient) {
     };
     if (patient["referredBy"].value == "Hospital") {
         returnValues.show.push("hospitalSection");
-        returnValues.hide.push("doctorSection");
-        returnValues.hide.push("pharmacySection");
-        returnValues.hide.push("promotionalActivitySection");
+        returnValues.hide.push("doctorSection", "pharmacySection", "promotionalActivitySection");
     } else if (patient["referredBy"].value == "Pharmacy") {
         returnValues.show.push("pharmacySection");
-        returnValues.hide.push("doctorSection");
-        returnValues.hide.push("hospitalSection");
-        returnValues.hide.push("promotionalActivitySection");
+        returnValues.hide.push("doctorSection", "hospitalSection", "promotionalActivitySection");
     } else if (patient["referredBy"].value == "Promotional Activity") {
         returnValues.show.push("promotionalActivitySection");
-        returnValues.hide.push("doctorSection");
-        returnValues.hide.push("hospitalSection");
-        returnValues.hide.push("pharmacySection");
+        returnValues.hide.push("doctorSection", "hospitalSection", "pharmacySection");
     } else if (patient["referredBy"].value == "Doctor") {
         returnValues.show.push("doctorSection");
-        returnValues.hide.push("hospitalSection");
-        returnValues.hide.push("pharmacySection");
-        returnValues.hide.push("promotionalActivitySection");
+        returnValues.hide.push("hospitalSection", "pharmacySection", "promotionalActivitySection");
     } else {
         returnValues.hide.push("hospitalSection", "doctorSection", "pharmacySection", "promotionalActivitySection");
     }
@@ -59,17 +51,13 @@ Bahmni.Registration.AttributesConditions.rules = {
         };
         if (patient["patientCategory"].value == "MF") {
             returnValues.show.push("mfidSection");
-            returnValues.hide.push("healthIdSection");
-            returnValues.hide.push("staffIdSection");
+            returnValues.hide.push("healthIdSection", "staffIdSection");
         } else if (patient["patientCategory"].value == "Health card holder") {
             returnValues.show.push("healthIdSection");
-            returnValues.hide.push("mfidSection");
-            returnValues.hide.push("staffIdSection");
-
+            returnValues.hide.push("mfidSection", "staffIdSection");
         } else if (patient["patientCategory"].value == "Staff, Self & family") {
             returnValues.show.push("staffIdSection");
-            returnValues.hide.push("mfidSection");
-            returnValues.hide.push("healthIdSection");
+            returnValues.hide.push("mfidSection", "healthIdSection");
         } else {
             returnValues.hide.push("staffIdSection", "mfidSection", "healthIdSection");
         }
