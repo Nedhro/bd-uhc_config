6981e64d901a33181799896ba542960abb6c136f
akter_rakib <rakibccj@gmail.com>
2019-11-14 11:05:46 +0600

    obs- consent form added

---
+++ sql/openmrs/observation/consent.sql
@@ -0,0 +1,9 @@
+call add_concept(@concept_id, @s_name_id, @f_name_id, "DoctorsName", "Name", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "AnesthesiologistName", "Anesthesiologist Name", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "GuardiansName", "Name", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "RelationWithPatient", "Relation with patient", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "GuardiansAddress", "Address", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "GuardiansPhoneNo", "Phone No.", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "WitnessName", "Name", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "WitnessAddress", "Address", "Text", "Misc", false);
+call add_concept(@concept_id, @s_name_id, @f_name_id, "WitnessPhoneNo", "Phone No.", "Text", "Misc", false);
