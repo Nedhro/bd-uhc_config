b045aba1ccd180ef95433a719e63f9884468f253
nedhro <nedhror@gmail.com>
2020-06-10 15:46:39 +0600

    New file configured updated

--- openmrs/apps/reports/sql/doctorWisePatientList.sql
+++
@@ -1,5 +0,0 @@
-select concat(pn.given_name, ' ', pn.family_name) as DoctorName, count(rs.person_a) as NumberOfPatient from person_name as pn
-                                                                                                                inner join relationship as rs on rs.person_b=pn.person_id
-    and rs.relationship =(select rt.relationship_type_id from relationship_type as rt where a_is_to_b ='Doctor')
-    and cast(rs.date_created as DATE) between '#startDate#' AND '#endDate#'
-group by concat(pn.given_name, ' ', pn.family_name) order by count(rs.person_a) desc ;
--- openmrs/apps/reports/sql/thanaWisePatientList.sql
+++
@@ -1,4 +0,0 @@
-select  pa.address5 as 'Thana',count(v.visit_id) as 'Total Visit' from visit as v
-    inner join person_address as pa on pa.person_id = v.patient_id
-    and cast(v.date_created as DATE) between '#startDate#' AND '#endDate#'
-GROUP BY pa.address5 order by count(v.visit_id) desc;
